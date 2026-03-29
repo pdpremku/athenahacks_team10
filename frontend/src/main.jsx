ade082516fcf933f04f7738216a48502373347bf
Ruina Liu <ruinaliu2007@gmail.com>
2026-03-29 13:09:37 -0700

    cleaned main

--- frontend/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ClubAdvisor from "./ClubAdvisor";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ClubAdvisor />);
